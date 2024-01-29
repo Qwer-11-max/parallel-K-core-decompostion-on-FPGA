84acdc071ccc2b7c34c555788bcfba18bd482f4c
Qwer-11-max <1670470731@qq.com>
2024-01-29 16:36:55 +0800

    mid

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-#include <stdio.h>
+#include<iostream>
 
 int main(int argc, char *argv[])
 {
-    printf("hello world\n");
+    int a=1,b=2;
+    int c = a+b;
+
+    std::cout<<a<<"hello world"<<std::endl;
+    std::cout<<c<<std::endl;
     return 0;
 }
